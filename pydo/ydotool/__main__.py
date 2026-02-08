71ce1db2ea2168163454911437d57dec9a81a2f5
Jason Hsiao <oaishnosaj@gmail.com>
2026-02-08 14:05:52 +0800

    Adding a __main__ for pydo.ydotool

---
+++ pydo/ydotool/__main__.py
@@ -0,0 +1,25 @@
+import platform
+import argparse
+if platform.system() == 'Windows':
+    from .windows import ydotool, ydotoold
+else:
+    from .linux import ydotool, ydotoold
+
+
+
+def daemon(args):
+    """Run the ydotoold daemon."""
+    with ydotoold(socket=args.socket, verbose=args.verbose):
+        input('>>> ')
+
+
+p = argparse.ArgumentParser()
+s = p.add_subparsers()
+
+d = s.add_parser('daemon')
+d.set_defaults(handler=daemon)
+d.add_argument('-s', '--socket')
+d.add_argument('-v', '--verbose', action='store_true')
+
+args = p.parse_args()
+args.handler(args)
