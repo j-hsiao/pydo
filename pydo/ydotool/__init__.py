c450f46a5260b819b7bf1356037dd39e77bd92f1
Jason Hsiao <jason.hsiao@acer.com>
2026-02-09 10:57:19 +0800

    Update default imports.

--- pydo/ydotool/__init__.py
+++ pydo/ydotool/__init__.py
@@ -1,7 +1,8 @@
 """Basic ydotool functionality."""
+__all__ = ['ydotool', 'ydotoold']
 import platform
 if platform.system() == 'Windows':
-    from .windows import ydotool
+    from .windows import ydotool, ydotoold
 else:
-    from .linux import ydotool
+    from .linux import ydotool, ydotoold
 
