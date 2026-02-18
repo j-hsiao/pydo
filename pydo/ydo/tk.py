2250b14e190272c6cee148e7470ddc62e3d11966
Jason Hsiao <oaishnosaj@gmail.com>
2026-02-18 19:01:46 +0800

    updating.

--- pydo/ydo/tk.py
+++ pydo/ydo/tk.py
@@ -5,7 +5,7 @@ from . import _ydo
 
 import sys
 
-class TkYdo(_ydo.ydo, ydotool):
+class ydo(_ydo.ydo, ydotool):
     """Use tkinter to track mouse and keys."""
     class Active(_ydo.Active):
         def __init__(self, tool):
@@ -15,31 +15,48 @@ class TkYdo(_ydo.ydo, ydotool):
         def __enter__(self):
             r = self.tool.tk
             r.attributes('-fullscreen', True, '-topmost', True)
+            self.tool._configs.set(0)
             r.deiconify()
             r.call('vwait', self.tool._ready)
+            self.tool.click(self.tool.m.MIDDLE|self.tool.m.DOWN)
             return self
 
         def __exit__(self):
-            self.tool.click(self.tool.m.LEFT|self.tool.m.UP)
+            self.tool.click(self.tool.m.MIDDLE|self.tool.m.UP)
             self.r.withdraw()
 
     def __init__(self, *args, **kwargs):
         super(TkYdo, self).__init__(*args, **kwargs)
         self._active = self.Active(self)
         self._iter = None
+        self._state = 0
         self.tk = tk.Tk()
         self._pos = self.tk.winfo_pointerxy()
-        self._state = 0
 
+        self._ready = tk.BooleanVar(self.tk)
+        self._reached = tk.BooleanVar(self.tk)
+        self._configs = tk.IntVar(self.tk)
+
+        # TODO: 
+        # From observation:
+        #     system  <Motion>    <Configures>    <Configures> on <Enter>
+        #     Windows no          1               1
+        #     wsl     yes         6               5 (sometimes 6)
+        #     X (vnc) no          4               4
+        #     wayland yes*        2(sometimes 3)  2 (sometimes 3)
+        # *sometimes no... not consistent, not sure how to make it consistent.
+        # NOTE: maybe 0x0 is better than 1x1?
         self.tk.createcommand('OnMoveCallback', self._on_move)
         self.tk.createcommand('OnKeyCallback', self._on_key)
         self.tk.bind('<Motion>', 'OnMoveCallback %X %Y %s')
         self.tk.bind('<KeyPress>', 'OnKeyCallback %K %s')
-        self.tk.bind('<Enter>', '...') # TODO
-        self.tk.bind('<Configure>', '') # TODO
+        self.tk.bind(
+            '<Enter>',
+            'if {{${0} == 1 || ${0} == 4}} {{set {} 1\nset {0} 0}}'.format(self._configs))
+        self.tk.bind(
+            '<Configure>',
+            'set {0} [expr ${0} + 1]'.format(self._configs))
 
-        self._ready = tk.BooleanVar(self.tk)
-        self._reached = tk.BooleanVar(self.tk)
 
     def lockstate(self, key='Caps_Lock'):
         if isinstance(key, 'Caps_Lock':
