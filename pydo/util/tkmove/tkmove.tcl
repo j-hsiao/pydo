d870d24133d55a0ef1e93ea1f57945ebe222cd02
Jason Hsiao <oaishnosaj@gmail.com>
2026-03-30 23:45:21 +0800

    Update tkmove package.

--- pydo/util/tkmove/tkmove.tcl
+++ pydo/util/tkmove/tkmove.tcl
@@ -24,23 +24,21 @@ namespace eval ::tkmove {
 	bind tkmovetag <Motion> "::tkmove::moved %W"
 
 	proc wait_deiconify {window} {
-	# Leave window in topmost fullscreen.
+		# Leave window in topmost fullscreen.
 		set "::tkmove::count_${window}" 0
 		set "::tkmove::done_${window}" 0
 		puts "wait_deiconify ${window}"
 		try {
-			bindtags ${window} [lappend [bindtags ${window}] tkmovetag]
-			wm attributes $window -topmost true -fullscreen true
-			wm deiconify $window
-			after 1000 "set {::tkmove::done_${window}} 0"
+			bindtags "${window}" [lappend [bindtags "${window}"] tkmovetag]
+			wm attributes "${window}" -topmost true -fullscreen true
+			wm deiconify "${window}"
+			after 1000 [list set "::tkmove::done_${window}" 0]
 			vwait "::tkmove::done_${window}"
 			puts "wait done"
 		} finally {
-			bindtags ${window} [lrange [bindtags ${window}] 0 end-1]
+			bindtags "${window}" [lrange [bindtags "${window}"] 0 end-1]
 			unset "::tkmove::done_${window}"
 			unset "::tkmove::count_${window}"
 		}
 	}
 }
-
-::tkmove::wait_deiconify .
