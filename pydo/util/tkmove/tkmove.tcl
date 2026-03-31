3e1d9dd5dce2ebb5023a4dff5a61d3a1d172cc2b
Jason Hsiao <oaishnosaj@gmail.com>
2026-03-31 22:53:25 +0800

    Update tkmove.

--- pydo/util/tkmove/tkmove.tcl
+++ pydo/util/tkmove/tkmove.tcl
@@ -1,22 +1,35 @@
 package provide tkmove 0.1
 package require Tcl 8.6
 namespace eval ::tkmove {
+	variable verbose 0
 	proc enterred {window} {
 		upvar "::tkmove::count_${window}" cfgcount
-		puts "enterred ${cfgcount}"
+		if {$::tkmove::verbose} {
+			puts -nonewline "E"
+			flush stdout
+		}
+		if {${cfgcount} == 1 || ${cfgcount} == 4} {
+			set "::tkmove::done_${window}" 0
+		}
 	}
 
 	proc configured {window} {
 		upvar "::tkmove::count_${window}" cfgcount
 		set cfgcount [expr "${cfgcount}+1"]
-		puts "configured ${cfgcount}"
+		if {$::tkmove::verbose} {
+			puts -nonewline "C"
+			flush stdout
+		}
 	}
 
 	proc moved {window} {
 		upvar "::tkmove::count_${window}" cfgcount
 		upvar "::tkmove::done_${window}" cfgdone
-		puts "moved ${cfgcount}"
 		set cfgdone 0
+		if {$::tkmove::verbose} {
+			puts -nonewline "M"
+			flush stdout
+		}
 	}
 
 	bind tkmovetag <Configure> "::tkmove::configured %W"
@@ -27,14 +40,19 @@ namespace eval ::tkmove {
 		# Leave window in topmost fullscreen.
 		set "::tkmove::count_${window}" 0
 		set "::tkmove::done_${window}" 0
-		puts "wait_deiconify ${window}"
+		if {$::tkmove::verbose} {
+			puts "wait_deiconify ${window}"
+		}
 		try {
 			bindtags "${window}" [lappend [bindtags "${window}"] tkmovetag]
 			wm attributes "${window}" -topmost true -fullscreen true
 			wm deiconify "${window}"
-			after 1000 [list set "::tkmove::done_${window}" 0]
+			variable afterev [after 1000 [list set "::tkmove::done_${window}" 0]]
 			vwait "::tkmove::done_${window}"
-			puts "wait done"
+			after cancel ${afterev}
+			if {$::tkmove::verbose} {
+				puts ""
+			}
 		} finally {
 			bindtags "${window}" [lrange [bindtags "${window}"] 0 end-1]
 			unset "::tkmove::done_${window}"
